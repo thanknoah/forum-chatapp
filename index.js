4ee4496137d5d640e427924bda27963e960497ee
thanknoah <78211701+thanknoah@users.noreply.github.com>
2024-07-02 20:37:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var http = require('http').createServer(app);
 var io = require("socket.io")(http, {
-    cors: { origin: "https://thenoahdevs.com/forum/chat.html" }
+    cors: { origin: "https://thenoahdevs.com" }
 });
 
 let serverList = []
