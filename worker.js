3652893a55a1c6eb2aa7e0d2cd790f5a779f1942
thanknoah <78211701+thanknoah@users.noreply.github.com>
2024-07-13 23:24:14 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,16 @@
+// Imports
 const { 
     workerData, 
     parentPort 
 } = require('worker_threads');
 const HashMap = require('hashmap');
-const { Console } = require('console');
 
+// Handle Work
 parentPort.on('message', (numMsg) => {
     let map = new HashMap(); 
     map.copy(numMsg[0]);
 
+    // Handle Server Startup
     if (numMsg[2] == "startup") {
         setInterval(() => {
             let y = map.keys();
@@ -19,11 +21,11 @@ parentPort.on('message', (numMsg) => {
             for (let x = 0; x < numMsg[1]; x++) {
                 maps.set(numMsg[1][x], 0)
             }
-
             parentPort.postMessage(map);
         }, 120000);
     } 
 
+    // Set Cooldown for client
     if (numMsg[2] == "setCoolDown") {
         setTimeout(() => {
             map.set(numMsg[1], 0);
